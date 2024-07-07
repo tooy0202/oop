b55f5be2a43d23a7fc2bd6b3f0c70eb4100f3a19
Athit Suntalodom <118352521+tooy0202@users.noreply.github.com>
2024-07-07 20:51:48 +0700

    Delete Lab6.py

--- Lab6.py
+++
@@ -1,8 +0,0 @@
-name = ["a", "b", "c"]
-ages = [25, 30, 35]
-city = ["New York", "Los Angeles", "thailand"]
-
-for name, ages, city in zip(name,ages,city):
-    #zip ทำให้สามารถวนได้พร้อมกัน
-    print(f"{name} is {ages} years old and lives in {city}")
-#f คือ แสดงค่าในตัวแปรแต่ละตัว
