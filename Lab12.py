abb453687882a418b7a12e8200178356cd626c9b
Athit Suntalodom <118352521+tooy0202@users.noreply.github.com>
2024-07-07 20:51:13 +0700

    Delete Lab12.py

--- Lab12.py
+++
@@ -1,8 +0,0 @@
-cities = {
-    'New York':[1,2,3,4,5,6],
-    'Chaicago':[7,8,9,10,11,12],
-    'thailand':[13,14,15,16,17,18]
-}
-
-av = {city:sum(temps)/len(temps) for city,temps in cities.items()}
-print("Ave:",av)
